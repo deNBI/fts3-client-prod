b7462ec7d27a4e9d57faf5a283d316c4658bc43a
ekatchko <ewgenijk@gmx.de>
2022-11-25 01:42:27 +0100

    escape quotes

--- app/transformers/minio.py
+++ app/transformers/minio.py
@@ -7,7 +7,7 @@ async def jobs_to_minio_exclude_string(jobs: List[Job]):
     exclude_string = ""
     for job in jobs:
         if job.job_metadata.path:
-            exclude_string += f"--exclude \"/{job.job_metadata.database}/{job.job_metadata.version}/{job.job_metadata.path}/*\" "
+            exclude_string += f"--exclude \042/{job.job_metadata.database}/{job.job_metadata.version}/{job.job_metadata.path}/*\042 "
         else:
-            exclude_string += f"--exclude \"/{job.job_metadata.database}/{job.job_metadata.version}/*\" "
+            exclude_string += f"--exclude \042/{job.job_metadata.database}/{job.job_metadata.version}/*\042 "
     return exclude_string
