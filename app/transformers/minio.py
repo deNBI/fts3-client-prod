fe36af9a70e103da51289819ff2c1303540e3f04
ekatchko <ewgenijk@gmx.de>
2022-11-25 01:39:39 +0100

    multiple excludes in command

--- app/transformers/minio.py
+++ app/transformers/minio.py
@@ -4,10 +4,10 @@ from app.models.serializers import Job
 
 
 async def jobs_to_minio_exclude_string(jobs: List[Job]):
-    exclude_string = "--exclude "
+    exclude_string = ""
     for job in jobs:
         if job.job_metadata.path:
-            exclude_string += f"/{job.job_metadata.database}/{job.job_metadata.version}/{job.job_metadata.path}/*"
+            exclude_string += f"--exclude \"/{job.job_metadata.database}/{job.job_metadata.version}/{job.job_metadata.path}/*\" "
         else:
-            exclude_string += f"/{job.job_metadata.database}/{job.job_metadata.version}/*"
+            exclude_string += f"--exclude \"/{job.job_metadata.database}/{job.job_metadata.version}/*\" "
     return exclude_string
